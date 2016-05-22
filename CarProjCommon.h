fcdad9fdd2f3c28df875eaacd110ffce99086a16
Michael Ackerman <binarymac@gmail.com>
2016-05-22 16:56:36 -0700

    moved includes into single file, added folders

---
+++ CarProjCommon.h
@@ -0,0 +1,9 @@
+#ifndef "CARPROJCOMMON_H"
+#define "CARPROJCOMMON_H"
+
+#include "constants.h"
+#include "Cstring/Cstring.h"
+#include "StringStream/StringStream.h"
+#include "Packet/Packet.h"
+
+#endif //CARPROJCOMMON_H
